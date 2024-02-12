b54534cb1645f8b95121c5b795fb9350e4513b36
Manjunath-6499 <Manjumadesh16@gmail.com>
2024-02-12 20:35:46 +0530

    created alert dialog box

--- lib/widgets/expense_list/new_expense.dart
+++ lib/widgets/expense_list/new_expense.dart
@@ -1,3 +1,4 @@
+import "package:expensive_tracker_app/model/expense.dart";
 import "package:flutter/material.dart";
 
 class NewExpense extends StatefulWidget {
@@ -8,15 +9,159 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titlecontroller = TextEditingController();
+  final _amountcontroller = TextEditingController();
+  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+
+  void _presentDatePicker() async {
+    final now = DateTime.now();
+
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+
+    final pickedDate = await showDatePicker(
+      context: context,
+      firstDate: firstDate,
+      lastDate: now,
+    );
+
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
+
+  @override
+  void dispose() {
+    _titlecontroller.dispose();
+    _amountcontroller.dispose();
+    super.dispose();
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(
+      _amountcontroller.text,
+    ); //tryparse("hello") => null, try parse ("1.12") => 1.12
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titlecontroller.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Invalid input"),
+          content: const Text(
+            "Please make sure a valid title , amount, date and category was entered..",
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text(
+                "Close",
+              ),
+            )
+          ],
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16.0),
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
       child: Column(
         children: [
           TextField(
+            controller: _titlecontroller,
             maxLength: 50,
-            decoration: InputDecoration(label: Text("Title")),
+            decoration: const InputDecoration(
+              label: Text(
+                "Title",
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountcontroller,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: "\$ ",
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      _selectedDate == null
+                          ? "No date selected"
+                          : formatter.format(
+                              _selectedDate!,
+                            ),
+                    ),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              DropdownButton(
+                  value: _selectedCategory,
+                  items: Category.values
+                      .map(
+                        (Category) => DropdownMenuItem(
+                          value: Category,
+                          child: Text(
+                            Category.name.toUpperCase(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                  onChanged: (value) {
+                    if (value == null) {
+                      return;
+                    }
+                    setState(() {
+                      _selectedCategory = value;
+                    });
+                  }),
+              const Spacer(),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _submitExpenseData();
+                },
+                child: const Text(
+                  "Save Expense",
+                ),
+              ),
+            ],
           )
         ],
       ),
